8cd7a011e0c947a8f3c2b1d46e84f01cfe8963e2
Steven Sinatra <ohai@diagramatics.me>
2015-03-30 02:48:32 +1100

    Build system with Gulp!

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var del = require('del');
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
 var wiredep = require('wiredep').stream;
 var $ = require('gulp-load-plugins')();
@@ -17,15 +18,19 @@ gulp.task('styles', function() {
     }))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/css'))
+    .pipe($.size({title: 'styles'}))
     .pipe(browserSync.reload({
       stream: true
     })
   );
 });
 
-gulp.task('bower', function() {
-  gulp.src('app/index.html')
-    .pipe(wiredep({}))
+gulp.task('wiredep', function() {
+  return gulp.src('app/index.html')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)*\.\./,
+      exclude: ['bower_components/modernizr/modernizr.js'],
+    }))
     .pipe(gulp.dest('app'));
 });
 
@@ -37,22 +42,83 @@ gulp.task('copy', function () {
 });
 
 gulp.task('watch', function() {
-  browserSync.init({
-    server: ['.tmp', 'app']
+  browserSync({
+    notify: false,
+    server: ['.tmp', 'app'],
+    routes: {
+      '/bower_components': 'bower_components'
+    }
   });
   gulp.watch('app/scss/**/*.scss', ['styles']);
-  gulp.watch('app/index.html', browserSync.reload);
-  gulp.watch('images/**/*.{png, jpg, svg}', browserSync.reload);
+  gulp.watch('bower.json', ['wiredep']);
+  gulp.watch([
+    'app/index.html',
+    'images/**/*.**'
+  ]).on('change', browserSync.reload);
 });
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
-gulp.task('dev', ['clean', 'styles', 'watch'], function() {
+// Optimize images
+gulp.task('images', function () {
+  return gulp.src([
+    'app/images/**/*',
+    '!app/images/icons.svg'
+  ])
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size({title: 'images'}));
+});
+
+gulp.task('html', function() {
+  var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
+  return gulp.src('app/index.html')
+    .pipe(assets)
+    // Concatenate and minify JavaScript files
+    .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
+    // Remove unused styles
+    // TODO: Disabled now since it doesn't work well with Modernizr's classes
+    // .pipe($.if('*.css', $.uncss({
+    //   html: [
+    //     'app/index.html'
+    //   ],
+    // })))
+    // Minify CSS
+    .pipe($.if('*.css', $.csso()))
+		.pipe($.rev())
+    .pipe(assets.restore())
+    .pipe($.useref())
+		.pipe($.revReplace())
+    // Minify HTML
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    // Output files
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'html'}));
+});
+
+gulp.task('dev', ['clean'], function() {
+  runSequence('styles', ['wiredep', 'watch']);
   browserSync.reload();
 });
 
-gulp.task('build', [], function() {
+gulp.task('build', ['clean', 'wiredep'], function() {
+  runSequence('styles', ['html', 'images'], function() {
+    return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+  });
+});
 
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['build'], function () {
+  browserSync({
+    notify: false,
+    server: 'dist'
+  });
 });
 
 gulp.task('default', ['build']);
