60fb0c3300901929735de14072e5bdde326fe6b1
Steven Sinatra <ohai@diagramatics.me>
2015-03-29 22:34:18 +1100

    Use 'app' and '.tmp' for structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,41 @@ var browserSync = require('browser-sync');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function() {
-  return $.rubySass('scss/style.scss', {
-      style: 'nested',
-      precision: 10,
-      defaultEncoding: 'UTF-8',
-      sourcemap: true
-    })
-    .pipe($.autoprefixer({
+  return $.rubySass('app/scss/style.scss', {
+    style: 'expanded',
+    precision: 10,
+    defaultEncoding: 'UTF-8',
+    sourcemap: true
+  }).pipe($.autoprefixer({
       browsers: ['last 2 version', 'ie 8', 'ie 9']
     }))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('.tmp/css'))
     .pipe(browserSync.reload({
       stream: true
-    }));
+    })
+  );
+});
+
+gulp.task('copy', function () {
+  return gulp.src(['app/images'], {
+    dot: true
+  }).pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'copy'}));
 });
 
 gulp.task('watch', function() {
   browserSync.init({
-    server: {
-      baseDir: './',
-      directory: true
-    }
+    server: ['.tmp', 'app']
   });
+  gulp.watch('app/scss/**/*.scss', ['styles']);
+  gulp.watch('app/index.html', browserSync.reload);
+  gulp.watch('images/**/*.{png, jpg, svg}', browserSync.reload);
 });
 
-gulp.task('clean', del.bind(null, ['dist/*', '!dist/.git'], {dot: true}));
+gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
-gulp.task('dev', ['styles', 'watch'], function() {
-  gulp.watch('scss/**/*.scss', ['styles']);
-  gulp.watch('index.html', browserSync.reload);
+gulp.task('dev', ['clean', 'styles', 'watch'], function() {
   browserSync.reload();
 });
 
